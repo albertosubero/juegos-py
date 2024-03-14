a0c0e3035c9f96fe0638a5fdcb9cc6117454e020
avsg1991@gmail.com <avsg1991@gmail.com>
2024-03-14 13:06:03 -0400

    JPY: add CategoryViewComponent

--- src/app/views/category-view/category-view.component.ts
+++ src/app/views/category-view/category-view.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { GamesService } from '../../shared/services/games.service';
+import { GameModel } from '../../shared/models/games';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './category-view.component.html',
   styleUrl: './category-view.component.scss'
 })
 export class CategoryViewComponent {
+  gameCategory: string = ''
+  games: GameModel[] = []
 
+  constructor(private route: ActivatedRoute, private gamesService: GamesService) {
+    this.gameCategory = this.route.snapshot.params['category_name']
+  }
+
+  ngOnInit() {
+    this.getGamesByCategory()
+  }
+
+  getGamesByCategory() {
+    this.gamesService.findGamesByFilters({categories: this.gameCategory})
+    .subscribe({
+      next: (res) => {
+        this.games = res;
+      },
+      error: (err) => {
+        console.error(err.error);
+      }
+    });
+  }
 }
