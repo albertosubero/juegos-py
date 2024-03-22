f8b2a415ecbe0f8248f2e75fd0c6b913dd83d146
avsg1991@gmail.com <avsg1991@gmail.com>
2024-03-22 12:13:26 -0400

    JPY: add about us and policy views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,21 @@ import { HomeViewComponent } from './views/home-view/home-view.component';
 import { GameViewComponent } from './views/game-view/game-view.component';
 import { CategoryViewComponent } from './views/category-view/category-view.component';
 import { SearchResultsViewComponent } from './views/search-results-view/search-results-view.component';
+import { AboutUsComponent } from './views/about-us/about-us.component';
+import { PrivacyPolicyComponent } from './views/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeViewComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyComponent
   },
   {
     path: 'game/:category/:id',
